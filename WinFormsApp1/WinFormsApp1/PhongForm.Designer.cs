891c908dcb1baec3d2d433b7a42f5aa2c36e9ae5
quocdat22 <hoanglequocdat132002@gmail.com>
2023-10-02 22:00:01 +0700

    Dat_10/2_PhongForm_10:00
    Thêm, xóa, sửa, data binding

--- WinFormsApp1/WinFormsApp1/PhongForm.Designer.cs
+++ WinFormsApp1/WinFormsApp1/PhongForm.Designer.cs
@@ -29,34 +29,131 @@
         private void InitializeComponent()
         {
             dataGridView1 = new DataGridView();
+            label1 = new Label();
+            label2 = new Label();
+            txtSoPhong = new TextBox();
+            txtLoaiPhong = new TextBox();
+            btnThem = new Button();
+            btnXoa = new Button();
+            btnSua = new Button();
+            button4 = new Button();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
             // dataGridView1
             // 
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(116, 296);
+            dataGridView1.Location = new Point(116, 291);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.RowHeadersWidth = 51;
             dataGridView1.RowTemplate.Height = 29;
             dataGridView1.Size = new Size(777, 188);
             dataGridView1.TabIndex = 0;
+            dataGridView1.CellClick += dataGridView1_CellClick;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(117, 78);
+            label1.Name = "label1";
+            label1.Size = new Size(72, 20);
+            label1.TabIndex = 1;
+            label1.Text = "Số Phòng";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(117, 160);
+            label2.Name = "label2";
+            label2.Size = new Size(83, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Loại Phòng";
+            // 
+            // txtSoPhong
+            // 
+            txtSoPhong.Location = new Point(217, 75);
+            txtSoPhong.Name = "txtSoPhong";
+            txtSoPhong.Size = new Size(194, 27);
+            txtSoPhong.TabIndex = 2;
+            // 
+            // txtLoaiPhong
+            // 
+            txtLoaiPhong.Location = new Point(217, 157);
+            txtLoaiPhong.Name = "txtLoaiPhong";
+            txtLoaiPhong.Size = new Size(194, 27);
+            txtLoaiPhong.TabIndex = 2;
+            // 
+            // btnThem
+            // 
+            btnThem.Location = new Point(117, 227);
+            btnThem.Name = "btnThem";
+            btnThem.Size = new Size(94, 29);
+            btnThem.TabIndex = 3;
+            btnThem.Text = "Thêm";
+            btnThem.UseVisualStyleBackColor = true;
+            btnThem.Click += btnThem_Click;
+            // 
+            // btnXoa
+            // 
+            btnXoa.Location = new Point(217, 227);
+            btnXoa.Name = "btnXoa";
+            btnXoa.Size = new Size(94, 29);
+            btnXoa.TabIndex = 3;
+            btnXoa.Text = "Xóa";
+            btnXoa.UseVisualStyleBackColor = true;
+            btnXoa.Click += button2_Click;
+            // 
+            // btnSua
+            // 
+            btnSua.Location = new Point(317, 227);
+            btnSua.Name = "btnSua";
+            btnSua.Size = new Size(94, 29);
+            btnSua.TabIndex = 3;
+            btnSua.Text = "Sửa";
+            btnSua.UseVisualStyleBackColor = true;
+            btnSua.Click += btnSua_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(799, 520);
+            button4.Name = "button4";
+            button4.Size = new Size(94, 29);
+            button4.TabIndex = 3;
+            button4.Text = "Sinh Viên";
+            button4.UseVisualStyleBackColor = true;
             // 
             // PhongForm
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1080, 588);
+            Controls.Add(button4);
+            Controls.Add(btnSua);
+            Controls.Add(btnXoa);
+            Controls.Add(btnThem);
+            Controls.Add(txtLoaiPhong);
+            Controls.Add(txtSoPhong);
+            Controls.Add(label2);
+            Controls.Add(label1);
             Controls.Add(dataGridView1);
             Name = "PhongForm";
             Text = "PhongForm";
             Load += PhongForm_Load;
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private DataGridView dataGridView1;
+        private Label label1;
+        private Label label2;
+        private TextBox txtSoPhong;
+        private TextBox txtLoaiPhong;
+        private Button btnThem;
+        private Button btnXoa;
+        private Button btnSua;
+        private Button button4;
     }
 }
--- WinFormsApp1/WinFormsApp1/PhongForm.cs
+++ WinFormsApp1/WinFormsApp1/PhongForm.cs
@@ -13,28 +13,176 @@ namespace WinFormsApp1
 {
     public partial class PhongForm : Form
     {
+        private MongoClient client;
+        private IMongoDatabase database;
+        private IMongoCollection<Phong> collection;
+
         public PhongForm()
         {
-            
+            client = new MongoClient("mongodb://localhost:27017");
+            database = client.GetDatabase("do_an");
+            collection = database.GetCollection<Phong>("QLDienNuoc");
+
             InitializeComponent();
         }
 
         private void PhongForm_Load(object sender, EventArgs e)
         {
             LoadDataPhong();
-            
+
         }
+
 
         public void LoadDataPhong()
         {
-            MongoClient client = new MongoClient("mongodb://localhost:27017");
-            IMongoDatabase database = client.GetDatabase("do_an");
-            IMongoCollection<Phong> collection = database.GetCollection<Phong>("QLDienNuoc");
 
             var phongList = collection.Find(_ => true).ToList();
 
             dataGridView1.DataSource = phongList;
             dataGridView1.Columns["_id"].Visible = false;
+        }
+
+        public void btnThem_Click(object sender, EventArgs e)
+        {
+            string soPhong = txtSoPhong.Text;
+
+            int loaiPhong;
+            if (int.TryParse(txtLoaiPhong.Text, out loaiPhong))
+            {
+
+                var filter = Builders<Phong>.Filter.Eq(x => x.soPhong, soPhong);
+                var existingRoom = collection.Find(filter).FirstOrDefault();
+
+                if (existingRoom != null)
+                {
+                    MessageBox.Show("Phòng đã tồn tại. Vui lòng sử dụng số phòng khác.");
+                    txtSoPhong.Clear();
+                    txtSoPhong.Focus();
+                    return;
+                }
+
+                if (string.IsNullOrWhiteSpace(soPhong) || loaiPhong <= 0)
+                {
+                    MessageBox.Show("Số phòng không được để trống và Loại phòng phải là số nguyên dương.");
+                    return;
+                }
+
+                Phong phong = new Phong
+                {
+                    soPhong = soPhong,
+                    loaiPhong = loaiPhong,
+                    sinhVien = new List<SinhVien>(),
+                    soDienNuoc = new List<SoDienNuoc>(),
+                    hoaDon = new List<HoaDon>()
+                };
+
+                collection.InsertOne(phong);
+
+                MessageBox.Show("Thêm phòng thành công!");
+                ClearForm();
+                LoadDataPhong();
+
+
+            }
+            else
+            {
+                MessageBox.Show("Loại phòng chỉ nhận chữ số");
+            }
+
+        }
+
+        public void ClearForm()
+        {
+            txtLoaiPhong.Clear();
+            txtSoPhong.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+
+                DataGridViewRow selectedRow = dataGridView1.SelectedRows[0];
+
+                string soPhong = selectedRow.Cells["soPhong"].Value.ToString();
+
+                var filter = Builders<Phong>.Filter.Eq(x => x.soPhong, soPhong);
+                var result = collection.DeleteOne(filter);
+
+                if (result.DeletedCount > 0)
+                {
+                    MessageBox.Show("Xóa Phòng thành công!");
+
+
+                    LoadDataPhong();
+                }
+                else
+                {
+                    MessageBox.Show("Không tìm thấy phòng có số: " + soPhong);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Chọn một phòng để xóa");
+            }
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+
+            string soPhong = txtSoPhong.Text;
+            int loaiPhong = Int32.Parse(txtLoaiPhong.Text);
+
+
+
+            var filter = Builders<Phong>.Filter.Eq(x => x.soPhong, soPhong);
+            var p = collection.Find(filter).FirstOrDefault();
+
+            if (p != null)
+            {
+
+                p.soPhong = soPhong;
+                p.loaiPhong = loaiPhong;
+
+
+
+                try
+                {
+                    collection.ReplaceOne(filter, p);
+                    MessageBox.Show("Cập nhật thông tin phòng thành công!");
+                    LoadDataPhong();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Lỗi khi cập nhật phongf: " + ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Không tìm thấy phòng có mã: " + soPhong);
+            }
+
+            ClearForm();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+           
+            if (e.RowIndex >= 0 && e.RowIndex < dataGridView1.Rows.Count)
+            {
+                
+                DataGridViewRow selectedRow = dataGridView1.Rows[e.RowIndex];
+
+                // Lấy giá trị của các ô trong hàng được chọn
+                string soPhong = selectedRow.Cells["soPhong"].Value.ToString();
+                string loaiPhong = selectedRow.Cells["loaiPhong"].Value.ToString();
+                
+
+                // Gán giá trị vào các trường nhập tương ứng
+                txtSoPhong.Text = soPhong;
+                txtLoaiPhong.Text = loaiPhong;
+
+            }
         }
     }
 }
